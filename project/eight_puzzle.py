ed6873d62947bc6c34cfb3f825a8c6a8c5c35c96
Masashi Yamaguchi <myama048@ucr.edu>
2021-11-05 21:10:30 -0700

    added depth and execution time

--- project/eight_puzzle.py
+++ project/eight_puzzle.py
@@ -9,6 +9,7 @@ try:
 except ImportError:
     import Queue as queue
 import numpy as np
+import time
 
 # ================================================================================================================
 # Testing Purpose
@@ -39,7 +40,6 @@ def compare_list(l1, l2): # returns true if 2 lists are equivalent otherwise ret
 def determine_move(node): # returns valid move based on the pos of blank (=0)
     blank = np.where(node == 0)
     slides = ['l', 'r', 'u', 'd']
-    # print('test', blank[0])
     if (blank[0] == 0):  # row 0
         if 'u' in slides:
             slides.remove('u')
@@ -68,173 +68,155 @@ def heuristic_misplaced_tile(node): # take curr node as input and return heurist
 def heuristic_manhattan_dist(node): # take curr node as input and return heuristic (manhattan_distance) value
     goal_state = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
     cnt = 0
-    #print('node', node)
+
     for row_idx in range(len(node)):
         for col_idx in range(len(node)):
             if (node[row_idx][col_idx] != goal_state[row_idx][col_idx]):
-                #print('dif at pos', row_idx, col_idx)
-                #print('node', node[row_idx][col_idx])
-                #print('goal', goal_state[row_idx][col_idx])
                 cp_row_idx = row_idx
                 cp_col_idx = col_idx
                 idx = np.where(goal_state == node[row_idx][col_idx]) # find idx of node[row_idx][col_idx] in goal_state
 
                 while (cp_row_idx != idx[0][0]):
-                    #print('in1')
-                    #print('while')
                     cnt += 1
                     if (cp_row_idx < idx[0][0].item()):
-                        #print('a')
                         cp_row_idx += 1
                     elif (cp_row_idx > idx[0][0].item()):
-                        #print('b')
                         cp_row_idx -= 1
 
                 while (cp_col_idx != idx[1][0].item()):
-                    #print('in2')
                     cnt += 1
                     if(cp_col_idx < idx[1][0].item()):
-                        #print('c')
                         cp_col_idx += 1
                     elif (cp_col_idx > idx[1][0].item()):
-                        #print('d')
                         cp_col_idx -= 1
-            #print('cnt', cnt)
 
     return cnt
 
-def uniform_cost_search(node):  # takes current node and return all its children with cost g(n) <-- = depth
+def uniform_cost_search(node):  # takes current node tuple (cost, depth, list) and return all its children with cost g(n) <-- = depth
     if not node:
         return []
 
-    blank = np.where(node[1] == 0)  # find '0' and return pos as (array([y]), array([x]))
-    slides = determine_move(node[1])
+    blank = np.where(node[2] == 0)  # find '0' and return pos as (array([y]), array([x]))
+    slides = determine_move(node[2])
     children = []
 
     if 'l' in slides:
-            new_state = np.copy(node[1])
-            #print(('new', new_state))
+            new_state = np.copy(node[2])
             copy = new_state[blank[0], blank[1]-1]
-            #print('copy' , copy[0])
             new_state[blank[0], blank[1]-1] = 0
             new_state[blank[0], blank[1]] = copy[0]
-            #print('after l: ', new_state)
-            children.append((node[0]+1, new_state)) # cost = cost(orig) + 1
-            #print('child', children)
+            children.append((node[0]+1, node[1]+1, new_state)) # cost = cost(orig) + 1
     if 'r' in slides:
-            new_state = np.copy(node[1])
-            #print('here',new_state)
+            new_state = np.copy(node[2])
             copy = new_state[blank[0], blank[1]+1]
             new_state[blank[0], blank[1]+1] = 0
             new_state[blank[0], blank[1]] = copy[0]
-            #print('after r: ', new_state)
-            children.append((node[0]+1, new_state))
-            #print('child', children)
+            children.append((node[0]+1, node[1]+1, new_state))
     if 'u' in slides:
-            new_state = np.copy(node[1])
+            new_state = np.copy(node[2])
             copy = new_state[blank[0] - 1, blank[1]]
             new_state[blank[0] - 1, blank[1]] = 0
             new_state[blank[0], blank[1]] = copy[0]
-            #print('after u: ', new_state)
-            children.append((node[0]+1, new_state))
+            children.append((node[0]+1, node[1]+1, new_state))
     if 'd' in slides:
-            new_state = np.copy(node[1])
+            new_state = np.copy(node[2])
             copy = new_state[blank[0]+1, blank[1]]
             new_state[blank[0]+1, blank[1]] = 0
             new_state[blank[0], blank[1]] = copy[0]
-            #print('after d: ', new_state)
-            children.append((node[0]+1, new_state))
+            children.append((node[0]+1, node[1]+1, new_state))
 
     return children
 
 def a_star_misplaced_tile(node):
     if not node:
         return []
-    blank = np.where(node[1] == 0)
-    slides = determine_move(node[1])
+    blank = np.where(node[2] == 0)
+    slides = determine_move(node[2])
     children = []
 
     if 'l' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[1])
         copy = new_state[blank[0], blank[1] - 1]
         new_state[blank[0], blank[1] - 1] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_misplaced_tile(old) + 1 + heuristic_misplaced_tile(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_misplaced_tile(new_state), node[1]+1, new_state))
     if 'r' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[1])
         copy = new_state[blank[0], blank[1] + 1]
         new_state[blank[0], blank[1] + 1] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_misplaced_tile(old) + 1 + heuristic_misplaced_tile(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_misplaced_tile(new_state), node[1]+1, new_state))
     if 'u' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[1])
         copy = new_state[blank[0] - 1, blank[1]]
         new_state[blank[0] - 1, blank[1]] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_misplaced_tile(old) + 1 + heuristic_misplaced_tile(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_misplaced_tile(new_state), node[1]+1, new_state))
     if 'd' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[2])
         copy = new_state[blank[0] + 1, blank[1]]
         new_state[blank[0] + 1, blank[1]] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_misplaced_tile(old) + 1 + heuristic_misplaced_tile(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_misplaced_tile(new_state), node[1]+1, new_state))
 
     return children
 
 def a_star_manhattan_dis(node):
     if not node:
         return []
-    blank = np.where(node[1] == 0)
-    slides = determine_move(node[1])
+    blank = np.where(node[2] == 0)
+    slides = determine_move(node[2])
     children = []
 
     if 'l' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[1])
         copy = new_state[blank[0], blank[1] - 1]
         new_state[blank[0], blank[1] - 1] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_manhattan_dist(old) + 1 + heuristic_manhattan_dist(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_manhattan_dist(new_state), node[1]+1, new_state))
     if 'r' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[1])
         copy = new_state[blank[0], blank[1] + 1]
         new_state[blank[0], blank[1] + 1] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_manhattan_dist(old) + 1 + heuristic_manhattan_dist(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_manhattan_dist(new_state), node[1]+1, new_state))
     if 'u' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[1])
         copy = new_state[blank[0] - 1, blank[1]]
         new_state[blank[0] - 1, blank[1]] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_manhattan_dist(old) + 1 + heuristic_manhattan_dist(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_manhattan_dist(new_state), node[1]+1, new_state))
     if 'd' in slides:
-        new_state = np.copy(node[1])
-        old = np.copy(node[1])
+        new_state = np.copy(node[2])
+        #old = np.copy(node[1])
         copy = new_state[blank[0] + 1, blank[1]]
         new_state[blank[0] + 1, blank[1]] = 0
         new_state[blank[0], blank[1]] = copy[0]
-        children.append((node[0] - heuristic_manhattan_dist(old) + 1 + heuristic_manhattan_dist(new_state), new_state))
+        children.append((node[1] + 1 + heuristic_manhattan_dist(new_state), node[1]+1, new_state))
 
     return children
 
 def general_search(problem, search_algo):
     goal_state = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
-    nodes = []  # (priority_number, data)
+    nodes = []  # (cost, depth, data)
+    # set up initial cost depending on search algorithm
     if search_algo == 1:
-        problem_tuple = (0, problem)
+        problem_tuple = (0, 0,problem)
     if search_algo == 2:
-        problem_tuple = (0 + heuristic_misplaced_tile(problem), problem)
+        problem_tuple = (0 + heuristic_misplaced_tile(problem),0, problem)
     if search_algo == 3:
-        problem_tuple = (0 + heuristic_manhattan_dist(problem), problem)
+        problem_tuple = (0 + heuristic_manhattan_dist(problem),0, problem)
 
     nodes.append(problem_tuple)
-    cnt = 0
+    cnt = 0 # = number of iterations
+    max_node = 0
 
     while True:
         cnt += 1
@@ -242,24 +224,27 @@ def general_search(problem, search_algo):
             print("failure")
             return -1
 
-        nodes.sort(key=lambda tup: tup[0]) # sort nodes by first element(cost)
+        nodes.sort(key=lambda tup: tup[0]) # sort nodes by their cost
+        max_node = max(max_node, len(nodes))
         node = nodes.pop(0)
         #print('node.head: ', node)
 
-        if compare_list(node[1], goal_state):
+        if compare_list(node[2], goal_state):
             print("succeed")
             print('Iteration Count: ', cnt)
-            return node
+            print('Depth: ', node[1])
+            print('Max Queue Size: ', max_node)
+            return node[2]
 
         if search_algo == 1:
             for item in uniform_cost_search(node):
-                nodes.append((item[0], np.array(item[1])))
+                nodes.append((item[0], item[1], np.array(item[2])))
         if search_algo == 2:
             for item in a_star_misplaced_tile(node):
-                nodes.append((item[0], np.array(item[1])))
+                nodes.append((item[0], item[1], np.array(item[2])))
         if search_algo == 3:
             for item in a_star_manhattan_dis(node):
-                nodes.append((item[0], np.array(item[1])))
+                nodes.append((item[0], item[1], np.array(item[2])))
 
 
         #print('nodes sz: ', len(nodes))
@@ -270,8 +255,8 @@ def general_search(problem, search_algo):
 # Main Function
 # ================================================================================================================
 def main():
-    print('Start')
-    search_algo = int(input("Type 1 for Uniform Cost Search, 2 for Heuristic(Misplaced Tile), 3 for Heuristic(Manhattan Distance): "))
+    print('Start\n')
+    search_algo = int(input("Enter 1 for Uniform Cost Search, 2 for Heuristic(Misplaced Tile), 3 for Heuristic(Manhattan Distance): "))
     choice = input('Do you want to use sample input: y or n: ')
 
     if choice == 'y':
@@ -283,7 +268,7 @@ def main():
         print('problem5 = np.array([[1,6,7], [5,0,3], [4,8,2]]) # depth 16')
         print('problem6 = np.array([[7,1,2], [4,8,5], [6,3,0]]) # depth 20')
         print('problem7 = np.array([[0,7,2], [4,6,1], [3,5,8]]) # depth 24')
-        val = int(input('Type 0 - 7: '))
+        val = int(input('Enter 0 - 7: '))
         if val == 0:
             problem = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
         if val == 1:
@@ -309,12 +294,18 @@ def main():
                 problem[i].append(item)
 
     problem = np.array(problem)
+    start = time.time()
     general_search(problem, search_algo)
-
-    print('End')
+    end = time.time()
+    print('Execution Time: ', end - start, 's')
+    print('\nEnd')
 
 if __name__ == "__main__":
     main()
 
 
 # end of code
+
+# ===== comment ======
+# return number of max nodes in queue
+# depth
